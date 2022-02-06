8aa85c28454e4fc5fdeba16f5d54e11f39c7ae32
Dung Dau <dudau0924222@gmail.com>
2022-02-06 13:35:14 -0500

    implement next.js server side rendering

--- medium-clone/pages/index.tsx
+++ medium-clone/pages/index.tsx
@@ -1,7 +1,16 @@
 import Head from 'next/head'
+import Link from 'next/link';
 import Header from '../components/Header'
+import { sanityClient, urlFor } from "../sanity";
+import { Post } from "../typings";
 
-export default function Home() {
+interface Props {
+  posts: Post[];
+}
+
+export default function Home({ posts }: Props) {
+  console.log(posts);
+  
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -28,8 +37,27 @@ export default function Home() {
           alt="" 
         />
       </div>
-
-      
     </div>
   )
 }
+
+export const getServerSideProps =async () => {
+  const query = `*[_type=="post"]{
+    _id,
+    title,
+    author -> {
+    name,
+    image
+    },
+    description,
+    mainImage,
+    slug
+  }`;
+
+  const posts = await sanityClient.fetch(query);
+  return {
+    props: {
+      posts,
+    },
+  };
+};

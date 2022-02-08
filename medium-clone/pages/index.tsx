e2a18640fb9eb85a1514b1fbf0e4e330dff3f0ec
Dung Dau <dudau0924222@gmail.com>
2022-02-07 19:10:01 -0500

    finish implementing the posts section

--- medium-clone/pages/index.tsx
+++ medium-clone/pages/index.tsx
@@ -8,9 +8,7 @@ interface Props {
   posts: Post[];
 }
 
-export default function Home({ posts }: Props) {
-  console.log(posts);
-  
+export default function Home({ posts }: Props) {  
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -37,8 +35,32 @@ export default function Home({ posts }: Props) {
           alt="" 
         />
       </div>
+
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:gap-6 p-2 md:p-6 lg:grid-cols-3'>
+        {posts.map((post) => (
+          <Link key={post._id} href={`/posts/${post.slug.current}`}>
+            <div className='border-rounded-lg group cursor-pointer overflow-hidden'>
+              <img
+                className='h-60 w-full object-cover group-hover: scale-105 transition-transform duration-200 ease-in-out' 
+                src={urlFor(post.mainImage).url()!} alt="" />
+              <div className='flex justify-between p-5 bg-white'>
+                <div>
+                  <p className='text-lg font-bold'>{post.title}</p>
+                  <p className='text-xs'>{post.description} by {post.author.name}</p>
+                  <p></p>
+                </div>
+                <img 
+                  className='h-12 w-12 rounded-full' 
+                  src={urlFor(post.author.image).url()!} 
+                  alt="" 
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export const getServerSideProps =async () => {
